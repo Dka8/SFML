570d24091d2e723c27d47252cd3179e3636fc165
Dka8 <wave_hos@mail.ru>
2017-12-03 01:46:22 +0300

    UdpSocket.hpp typo in comment for void unbind()

--- include/SFML/Network/UdpSocket.hpp
+++ include/SFML/Network/UdpSocket.hpp
@@ -96,8 +96,8 @@ public:
     /// \brief Unbind the socket from the local port to which it is bound
     ///
     /// The port that the socket was previously using is immediately
-    /// available after this function is called. If the
-    /// socket is not bound to a port, this function has no effect.
+    /// unavailable after this function is called.
+    /// If the socket is not bound to a port, this function has no effect.
     ///
     /// \see bind
     ///
